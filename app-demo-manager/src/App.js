321fedaa5dbec8b08e6b2286481a0d9a685bc6aa
Lê Bá Lộc <lochuedvfb@gmail.com>
2024-01-09 11:13:20 +0700

    cup components

---
+++ app-demo-manager/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Maincontent from './components/Maincontent/MainContent';
+
+function App() {
+  return (
+    <div className="App">
+      <Maincontent/>
+    </div>
+  );
+}
+
+export default App;
